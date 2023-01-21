d169e32fcc8980f7f730de9912366d432a1c396a
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2023-01-20 19:59:23 -0600

    assests added and theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors: {
+      'pale-robin-egg-blue': '#96D9CC',
+      'sonic-silver': '#747474'
+    },
     extend: {},
   },
   plugins: [],
